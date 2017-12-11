039df40cfd638d2facfe9c5e22c0ed910ab11af8
ctiller15 <christiller2015@gmail.com>
2017-12-11 01:39:34 -0500

    add in click transitions

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
-    /*
-     * Replace all SVG images with inline SVG
-     */
-        $('.svg').each(function(){
-            var $img = jQuery(this);
-            var imgID = $img.attr('id');
-            var imgClass = $img.attr('class');
-            var imgURL = $img.attr('src');
+/*
+* Replace all SVG images with inline SVG
+*/
+$('.svg').each(function(){
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
 
-            $.get(imgURL, function(data) {
-                // Get the SVG tag, ignore the rest
-                var $svg = jQuery(data).find('svg');
+    $.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = jQuery(data).find('svg');
 
-                // Add replaced image's ID to the new SVG
-                if(typeof imgID !== 'undefined') {
-                    $svg = $svg.attr('id', imgID);
-                }
-                // Add replaced image's classes to the new SVG
-                if(typeof imgClass !== 'undefined') {
-                    $svg = $svg.attr('class', imgClass+' replaced-svg');
-                }
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
 
-                // Remove any invalid XML tags as per http://validator.w3.org
-                $svg = $svg.removeAttr('xmlns:a');
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
 
-                // Replace image with new SVG
-                $img.replaceWith($svg);
+        // Replace image with new SVG
+        $img.replaceWith($svg);
 
-            }, 'xml');
+    }, 'xml');
 
-        });
+});
+
+var roofHeaders = document.querySelectorAll(".roof h4");
+var roofTexts = document.querySelectorAll(".roof p");
+console.log(roofHeaders);
+console.log(roofTexts);
+
+document.querySelectorAll(".roof").forEach((item, index) => {
+    item.addEventListener("click", (e) => {
+        console.log(e.target);
+        console.log(item);
+        console.log(roofHeaders[index]);
+        roofHeaders[index].classList.toggle("animated_header");
+        console.log(roofTexts[index]);
+        roofTexts[index].classList.toggle("animated_text");
+    });
+});
